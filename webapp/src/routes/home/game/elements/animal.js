05fda106b2d132d7c304f2208d008f0dcb17aa5a
jackowiak <matyskiewicz.j@gmail.com>
2019-03-19 21:29:58 +0100

    add food items

--- webapp/src/routes/home/game/elements/animal.js
+++ webapp/src/routes/home/game/elements/animal.js
@@ -1,8 +1,9 @@
 import { Container } from 'pixi.js';
 
-import { FENCES_INITIAL_Y, FENCES_ROWS, ANIMALS } from '../game.constans';
+import { FENCES_INITIAL_Y, FENCES_ROWS, ANIMALS, FOOD } from '../game.constans';
 import { AnimalLevel } from './animalLevel';
 import { AnimalHead } from './animalHead';
+import { FoodItem } from './foodItem';
 import { InterfaceText } from '../ui/interfaceText';
 import buffalo from '../../../../images/game/animals/animal-buffalo.png';
 import chicken from '../../../../images/game/animals/animal-chicken.png';
@@ -13,6 +14,15 @@ import goat from '../../../../images/game/animals/animal-goat.png';
 import moose from '../../../../images/game/animals/animal-moose.png';
 import pig from '../../../../images/game/animals/animal-pig.png';
 
+import foodBeef from '../../../../images/game/food/food-beef.png';
+import foodCheese from '../../../../images/game/food/food-cheese.png';
+import foodChicken from '../../../../images/game/food/food-chicken.png';
+import foodDuck from '../../../../images/game/food/food-duck.png';
+import foodEgg from '../../../../images/game/food/food-egg.png';
+import foodFrame from '../../../../images/game/food/food-frame.png';
+import foodHam from '../../../../images/game/food/food-ham.png';
+import foodMilk from '../../../../images/game/food/food-milk.png';
+import foodMoose from '../../../../images/game/food/food-moose.png';
 
 const textures = {
   [ANIMALS.buffalo]: buffalo,
@@ -25,8 +35,20 @@ const textures = {
   [ANIMALS.pig]: pig,
 };
 
+const foodTextures = {
+  [FOOD.beef]: foodBeef,
+  [FOOD.cheese]: foodCheese,
+  [FOOD.chicken]: foodChicken,
+  [FOOD.duck]: foodDuck,
+  [FOOD.egg]: foodEgg,
+  [FOOD.frame]: foodFrame,
+  [FOOD.ham]: foodHam,
+  [FOOD.milk]: foodMilk,
+  [FOOD.moose]: foodMoose,
+};
+
 export class Animal {
-  constructor({ rendererWidth, rendererHeight, type, positionNumber, amount, level }) {
+  constructor({ rendererWidth, rendererHeight, type, foodType, positionNumber, amount, level }) {
     this._stage = new Container();
     this._positionNum = this.getPositionNumber(positionNumber);
 
@@ -49,7 +71,12 @@ export class Animal {
 
     this.level = new AnimalLevel({ level: level, flip: !this.isEven(positionNumber) });
     this.animalHead = new AnimalHead({ type: textures[type], flip: !this.isEven(positionNumber) });
-    this.stage.addChild(this.level.stage, this.amount.stage, this.animalHead.stage);
+    this.foodItem = new FoodItem({
+      type: foodTextures[foodType],
+      x: this.isEven(positionNumber) ? 40 : -80,
+      y: -20,
+    });
+    this.stage.addChild(this.level.stage, this.amount.stage, this.animalHead.stage, this.foodItem.stage);
   }
 
   getPositionNumber = positionNumber => {
