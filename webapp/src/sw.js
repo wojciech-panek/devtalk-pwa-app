3fe3354665b78cfbea2d104d54a884da4e16bd19
Michał Kubiak <mkubiak@apptension.com>
2019-03-28 09:09:25 +0100

    Add image caching in sw

--- webapp/src/sw.js
+++ webapp/src/sw.js
@@ -6,3 +6,28 @@ self.addEventListener('activate', (event) => {
   console.log('sw activated');
   event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  const response = caches
+    .match(request)
+    .then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(request)
+        .then(resp => {
+          if (request.url.endsWith('.png') || request.url.endsWith('.jpg')) {
+            return caches.open('v1').then(cache => {
+              cache.put(request, resp.clone());
+              return resp;
+            });
+          }
+
+          return resp;
+        });
+    });
+  event.respondWith(response);
+});
