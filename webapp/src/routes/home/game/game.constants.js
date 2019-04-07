4623affe94e396de4ceabf7e94b7ce4682d34a31
jackowiak <matyskiewicz.j@gmail.com>
2019-04-07 15:44:00 +0200

    add icons and fix initial fences position

--- webapp/src/routes/home/game/game.constants.js
+++ webapp/src/routes/home/game/game.constants.js
@@ -18,7 +18,7 @@ import animalPigTexture from '../../../images/game/animals/animal-pig.png';
 
 export const FENCES_COLUMNS = 2;
 export const FENCES_ROWS = 4;
-export const FENCES_INITIAL_Y = 250;
+export const FENCES_INITIAL_Y = 215;
 export const UI_RADIUS = 10;
 
 export const ANIMALS = {
